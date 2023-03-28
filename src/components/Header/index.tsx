fafc6653023e7af50e61896104660547738e895b
Rafael Santos <rf.sants001@gmail.com>
2023-03-27 21:59:41 -0300

    feat: Create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <h2>next.CMS</h2>
+        <nav>
+          <a href="" className={styles.active}>
+            Home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/favicon.png"
+          type="image/png
+        "
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
