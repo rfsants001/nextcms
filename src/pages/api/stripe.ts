2a763fd46d773f8a19bd53bf060402703f1b9854
Rafael Santos <rf.sants001@gmail.com>
2023-03-28 21:36:58 -0300

    feat: Add Stripe API

---
+++ src/pages/api/stripe.ts
@@ -0,0 +1,15 @@
+import Stripe from 'stripe';
+import { version } from '../../../package.json'
+
+const stripeKey = process.env.STRIPE_API_KEY
+
+export const stripe = new Stripe(
+    stripeKey,
+    {
+        apiVersion: '2022-11-15',
+        appInfo: {
+            name: "Nextcms",
+            version
+        }
+    }
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,17 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
 import Image from "next/image";
 import { SubscribeButton } from "@/components/SubscribeButton";
+import { GetStaticProps } from "next";
+import { stripe } from "./api/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,11 +27,11 @@ export default function Home() {
             <span>React</span>.
           </h1>
           <p>
-            Get access to all the publications <br />{" "}
-            <span>for $9.00 per month</span>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <Image
           src="/images/hero.svg"
@@ -35,3 +44,21 @@ export default function Home() {
     </>
   );
 }
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1MqloVACnR4wCFpfLk9nqT1Z");
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount! / 100).toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    }),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  };
+};
